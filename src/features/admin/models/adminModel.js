cdc34a3a5228ce7aa502fede4635ac55fea6bb95
VAIBHAV ZADE <126239600+zade-vaibhav@users.noreply.github.com>
2025-01-07 02:28:30 +0530

    changing admin approval for admin

--- src/features/admin/models/adminModel.js
+++ src/features/admin/models/adminModel.js
@@ -6,6 +6,11 @@ const adminSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: "admin" }, // Specific to admins
+    adminApproval:{
+      type: String,
+      default: "Approved",
+      enum: ["Pending","Approved", "Reject"],
+    },
     reports: [
       {
         beneficiaryId: {
