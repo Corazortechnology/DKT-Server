5f1d418d43fae54890503945dc74213c33461178
VAIBHAV ZADE <126239600+zade-vaibhav@users.noreply.github.com>
2025-01-15 12:49:37 +0530

    adding email for asset request and assign

--- src/features/partner/controllers/acceptOrRejectAssetDelevery.js
+++ src/features/partner/controllers/acceptOrRejectAssetDelevery.js
@@ -1,11 +1,14 @@
+import { sendEmail } from "../../../services/emailService.js";
 import beneficiaryRequestModel from "../../beneficiary/models/beneficiaryRequestModel.js";
 import assetDelevery from "../models/assetDelevery.js";
+import partnerModel from "../models/partnerModel.js";
 
 
 export const acceptOrRejectAssetDelevery = async (req, res) => {
     try {
         const patner = req.userId
         const { requestId, status } = req.body;
+        const Patner = await partnerModel.findById(patner)
         const request = await assetDelevery.findById(requestId)
         if (!request) {
             return res.status(404).json({ success: false, message: "request not found" });
@@ -13,13 +16,15 @@ export const acceptOrRejectAssetDelevery = async (req, res) => {
         request.status = status;
         request.partnerId = patner;
 
-        const beneficeryRequest = await beneficiaryRequestModel.findById(request.beneficeryRequestId)
+        const beneficeryRequest = await beneficiaryRequestModel.findById(request.beneficeryRequestId).populate("beneficiaryId")
         if (!beneficeryRequest) {
             return res.status(404).json({ success: false, message: "BeneficeryRequest not found" });
         }
         beneficeryRequest.assignedDetails.status = "In-progress";
-        await beneficeryRequest.save()
+        await beneficeryRequest.save();
         await request.save();
+
+        await sendEmail(beneficeryRequest.beneficiaryId.email, "Allot Asset Delevery", { requestId: beneficeryRequest._id, deleveryPartner: Patner.partnerName })
         return res.status(200).json({ success: true, message: "Request accepted successfully!!" });
     } catch (error) {
         console.error(error);
